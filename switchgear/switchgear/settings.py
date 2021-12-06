449f9421101fc5648c05079b28e2b79029a1f789
solarden <pchraczynski@gmail.com>
2021-12-06 21:08:07 +0100

    Deploy refactor

--- switchgear/switchgear/settings.py
+++ switchgear/switchgear/settings.py
@@ -45,6 +45,7 @@ AUTH_USER_MODEL = 'tracker_app.Worker'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/logos')
