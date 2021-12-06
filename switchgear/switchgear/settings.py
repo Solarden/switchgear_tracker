e6b6addb0b0512012bf84b6f66238961d3c18c64
solarden <pchraczynski@gmail.com>
2021-12-06 21:20:19 +0100

    Deploy refactor

--- switchgear/switchgear/settings.py
+++ switchgear/switchgear/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'tracker_app/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'tracker_app/static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/logos')
 MEDIA_URL = '/media/logos/'
